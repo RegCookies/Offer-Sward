b9b4ed2b20dc21e20820a362c63b7f1b74306bc1
Regcookie <RegCookies@users.noreply.github.com>
2018-12-04 09:40:02 +0800

    Add files via upload

--- BinaryTree/Mirror_ reverse.py
+++ BinaryTree/Mirror_ reverse.py
@@ -11,5 +11,39 @@ def reverseTree(root):
     
     reverseTree(root.lchild)
     reverseTree(root.rchild)
-    
     root.lchild,root.rchild = root.rchild,root.lchild
+    
+def printT(root):
+    if root is None:
+        return None 
+    queue = [root]
+    result = []
+    while queue :
+        result.append(queue[0].val) 
+        if queue[0].lchild != None:
+            queue.append(queue[0].lchild)
+        if queue[0].rchild != None: 
+            queue.append(queue[0].rchild)
+        queue.pop(0)
+    return result
+if __name__ == "__main__":
+    root = BitNode(1) 
+    node1 = BitNode(2)
+    node2 = BitNode(3)
+    node3 = BitNode(4)
+    node4 = BitNode(5)
+    node5 = BitNode(6)
+    node6 = BitNode(7)
+
+    root.lchild = node1 
+    root.rchild = node2
+    node1.lchild = node3
+    node1.rchild = node4
+    node2.lchild = node5
+    node2.rchild = node6
+     
+    reverseTree(root)
+    
+    a = printT(root) 
+    
+    print(a)
